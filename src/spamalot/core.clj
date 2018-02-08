50b2d283bd7058b29abe56cc7d43cc524bf7be9a
Alan Thompson <cloojure@gmail.com>
2018-02-07 21:47:35 -0800

    misc

---
+++ src/spamalot/core.clj
@@ -0,0 +1,18 @@
+(ns spamalot.core
+  "Contains the core functions for namespace `spamalot.core`."
+  (:use tupelo.core)
+  (:require
+    [clojure.string :as str]
+    [schema.core :as s]
+  ))
+
+(s/defn mult :- s/Num
+  "Multiply two numbers"
+  [a :- s/Num
+   b :- s/Num ]
+  (* a b))
+
+(defn -main []
+  (println "main - enter")
+  )
+
---
+++ src/tst/spamalot/core.clj
@@ -0,0 +1,24 @@
+(ns tst.spamalot.core
+  (:use spamalot.core tupelo.core tupelo.test)
+  (:require 
+    [clojure.string :as str]
+    [clojure.java.io :as io] )
+  (:import [spamalot Calc] ))
+
+(dotest
+  (is= (spyx (+ 2 3)))
+  (throws? (/ 5 0))
+  (isnt false)
+
+  (let [crisis-txt (slurp (io/resource "thomas-paine.txt")) ]
+    (is (truthy? (re-find #"THESE are the times" crisis-txt))))
+
+  (is= 4   (mult 2 2))
+  (is= 5.0 (mult 2 2.5))
+
+  (is= 5.0 (spyx (Calc/add 2 3)))
+  (let [result (spyxx (spamalot.Calc/incVals {"able" 1 "baker" 2} )) ]
+    (is= {"able" 2 "baker" 3} result)
+    (is= java.util.HashMap (type result)))
+)
+
