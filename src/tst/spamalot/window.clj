d1ba26ed547a73c9a8f6c6297a6a6d588a346ed0
Alan Thompson <cloojure@gmail.com>
2018-02-08 17:04:34 -0800

    misc

--- src/tst/spamalot/window.clj
+++ src/tst/spamalot/window.clj
@@ -8,18 +8,18 @@
 (dotest
   (with-redefs [window-size-max 3]
     (nl) (window-reset!)
-    (pretty (forv [ii (thru 7)]
-              (do
-                (swap! window-atom add-email-to-window {:email-address "x" :spam-score ii})
-                [ii (mapv #(grab :spam-score %) @window-atom) (calc-window-spam-score @window-atom)])))
-    #_(is=
-      [[0 [0]]
-       [1 [0 1]]
-       [2 [0 1 2]]
-       [3 [1 2 3]]
-       [4 [2 3 4]]
-       [5 [3 4 5]]
-       [6 [4 5 6]]
-       [7 [5 6 7]]])))
+    (let [result (forv [ii (thru 7)]
+                   (do
+                     (swap! window-atom add-email-to-window {:email-address "x" :spam-score ii})
+                     [ii (mapv #(grab :spam-score %) @window-atom) (calc-window-spam-score @window-atom)]))]
+      (is= (spyx-pretty result)
+        [[0 [0] 0]
+         [1 [0 1] 1/2]
+         [2 [0 1 2] 1]
+         [3 [1 2 3] 2]
+         [4 [2 3 4] 3]
+         [5 [3 4 5] 4]
+         [6 [4 5 6] 5]
+         [7 [5 6 7] 6]]))))
 
 
